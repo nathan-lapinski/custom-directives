d740f6d0fc9641e9788aa4bffcbaade431141bf7
Nathan Lapinski <n4t311@gmail.com>
2018-06-25 17:28:17 +0900

    adding movable directive which extends draggable directive. need to investigate a bug with nested movables

---
+++ natelabs-drag-drop-directive/src/app/draggable/movable.directive.ts
@@ -0,0 +1,50 @@
+import { Directive, OnInit, HostListener, HostBinding, Host } from '@angular/core';
+import { DraggableDirective } from './draggable.directive';
+import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
+
+interface Position {
+  x: number;
+  y: number;
+}
+
+@Directive({
+  selector: '[appMovable]'
+})
+export class MovableDirective extends DraggableDirective {
+  @HostBinding('style.transform') get transform(): SafeStyle {
+    return this.sanitizer.bypassSecurityTrustStyle(
+      `translateX(${this.position.x}px) translateY(${this.position.y}px)`
+    );
+  }
+
+  @HostBinding('class.movable') movable = true;
+
+  private position: Position = { x: 0, y: 0};
+  private startPosition: Position;
+  private reset = true;
+
+  @HostListener('dragStart', ['$event'])
+  onDragStart(event: PointerEvent) {
+    this.startPosition = {
+      x: event.clientX - this.position.x,
+      y: event.clientY - this.position.y
+    };
+  }
+
+  @HostListener('dragMove', ['$event'])
+  onDragMove(event: PointerEvent) {
+    this.position.x = event.clientX - this.startPosition.x;
+    this.position.y = event.clientY - this.startPosition.y;
+  }
+
+  @HostListener('dragEnd', ['$event'])
+  onDragEnd(event: PointerEvent) {
+    if (this.reset) {
+      this.position = { x: 0, y: 0};
+    }
+  }
+
+  constructor(private sanitizer: DomSanitizer) {
+    super();
+  }
+}
